0790ae3da5329c9dc2976eaae915e21d109e4534
Zian Asti Dwiyanti <ziandwiasti23@gmail.com>
2022-12-11 15:52:46 +0700

    progress website

--- Website/app.py
+++ Website/app.py
@@ -5,3 +5,11 @@ app = Flask(__name__, static_url_path='/static')
 @app.route("/")
 def dashboard():
     return render_template('index.html')
+
+@app.route("/diagnosa")
+def diagnosa():
+    return render_template('diagnosa.html')
+
+@app.route("/history")
+def history():
+    return render_template('history.html')
